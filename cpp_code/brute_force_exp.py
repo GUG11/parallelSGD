fb843bd8f44768e1169aca74cae0a6c7e4b4e31b
GUG11 <hyzhang2112@gmail.com>
2016-11-23 10:42:22 -0600

    Initial experimental results

--- cpp_code/brute_force_exp.py
+++ cpp_code/brute_force_exp.py
@@ -1,6 +1,6 @@
 """
 Brute force enumeration on parallel minibatch SGD
-n = {1000,5000,10000,20000}  number of samples
+n = {1000,5000,10000}  number of samples
 d = {100, 200, 1000} dimension
 B = {100, 200, 500, 1000} batch size
 P = {2,4,8,16} number of cores
@@ -8,30 +8,40 @@ numIters = 100000
 learning rate = binary search on [1e-5, 5]
 """
 from subprocess import call
+import os
+import glob
 
 if __name__ == '__main__':
-    ns = [1000, 5000, 10000, 20000]
+    ns = [1000, 5000, 10000]
     ds = [100, 200, 1000]
     Bs = [100, 200, 500, 1000]
     Ps = [2, 4, 8, 16]
     schemes = ['random', 'corr']
     nit = 100000
     lo, hi, tol = 1e-5, 5, 0.1
+    filedir = os.path.join('..','results','simulations', 'Gaussian', 'minibatch')
     for scheme in schemes:
         for n in ns:
             for d in ds:
                 for B in Bs:
                     for P in Ps:
-                        lo, hi = 1e-5, 5
-                        ret = 1
-                        while 0.1 < hi - lo or ret != 0:
-                            mid = (lo + hi) / 2
-                            cmd = ['./par_minibatch_gaussian', str(n), str(d), str(P), str(mid), str(nit), str(B), scheme]
-                            print(' '.join(cmd))
-                            ret = call(cmd)
-                            if ret == 0:
-                                print('\n\nlearning rate %f converges!\n' % mid)
-                                lo = mid
-                            else:
-                                print('\n\nlearning rate %f diverges!\n' % mid)
-                                hi = mid
+                        fig = plt.figure(num=1, figsize=(20, 12))
+                        filepattern = '%s_n%d_d%d_B%d_T%d_gamma*_ths%d' % (scheme, n, d, B, nit, P)
+                        filepath = os.path.join(filedir, filepattern)
+                        filepathc = glob.glob(filepath)
+                        if len(filepathc) > 0:
+                            print(filepathc, ' exists')
+                        else:
+                            lo, hi = 1e-5, 5
+                            ret = 1
+                            while 0.1 < hi - lo or ret != 0:
+                                mid = (lo + hi) / 2
+                                cmd = ['./par_minibatch_gaussian', str(n), str(d), str(P), str(mid), str(nit), str(B), scheme]
+                                print(' '.join(cmd))
+                                ret = call(cmd)
+                                if ret == 0:
+                                    print('\n\nlearning rate %f converges!\n' % mid)
+                                    lo = mid
+                                else:
+                                    print('\n\nlearning rate %f diverges!\n' % mid)
+                                    hi = mid
---
+++ python_code/initial_study/simulation/plot_obj_time.py
@@ -0,0 +1,60 @@
+import numpy as np
+from misc import utils
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+import glob
+
+def parseData(contents):
+    lines = contents.split('\n')
+    epoches, times, losses = [], [], []
+    count = 0
+    for l in lines:
+        count += 1
+        data = filter(None, l.split(' '))
+        if count < 3 or len(data) != 3:
+            continue
+        epoches.append(int(data[0]))
+        times.append(float(data[1]))
+        losses.append(float(data[2]))
+    return epoches, times, losses
+
+
+if __name__ == '__main__':
+    matplotlib.rcParams.update({'font.size': 30})
+    ns = [1000, 5000, 10000]
+    ds = [100, 200, 1000]
+    Bs = [100, 200, 500, 1000]
+    Ps = [2, 4, 8, 16]
+    schemes = ['random', 'corr']
+    nit = 100000
+    lo, hi, tol = 1e-5, 5, 0.1
+    filedir = os.path.join('..','results','simulations', 'Gaussian', 'minibatch')
+    for n in ns:
+        for d in ds:
+            for B in Bs:
+                for P in Ps:
+                    fig = plt.figure(num=1, figsize=(20, 12))
+                    ax = fig.add_subplot(1,1,1)
+
+                    for scheme in schemes:
+                        filepattern = '%s_n%d_d%d_B%d_T%d_gamma*_ths%d' % (scheme, n, d, B, nit, P)
+                        filepath = os.path.join(filedir, filepattern)
+                        filepathc = glob.glob(filepath)
+                        if len(filepathc) == 0:
+                            raise Exception("File %s does not exist\n" % filepath)
+                        with open(filepathc[0], 'r') as ins:
+                            print('reading %s' % filepathc[0])
+                            contents = ins.read()
+                            epoches, times, losses = parseData(contents)
+                            x = np.array(epoches)
+                            y = np.log(np.array(losses))
+                            ax.plot(x, y, label=scheme, lw=2)
+
+                    utils.set_axis(ax, xlabel='iterations', ylabel=None, title='log(loss)', xticks=None, yticks=None, xlim=None, fontsize=30)
+                    plt.tight_layout()
+                    savefile = 'n%d_d%d_B%d_T%d_ths%d.svg' % (n, d, B, nit, P)
+                    save_dir = os.path.join(filedir, savefile)
+                    fig.savefig(save_dir)
+                    plt.cla()
+                    print('succeed saving %s' % save_dir)
